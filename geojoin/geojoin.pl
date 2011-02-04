b9d7668992f531652f8fa397e4aaabbc9eaabc0d
Kyle Isom <sadm@kyleisom.net>
2011-02-04 14:33:08 -0700

    add a couple of utility functions

--- geojoin/geojoin.pl
+++ geojoin/geojoin.pl
@@ -9,18 +9,64 @@ use vars qw($VERSION %IRSSI);
 use Geo::IP;
 
 # set up geoip city records file and irssi vars
-my $geocity_recfile = '/usr/local/share/GeoIP/GeoLiteCity.dat';
+my $city_recfile        = '/usr/local/share/GeoIP/GeoLiteCity.dat';
+my $use_city_records    = 'true';
+my $enabled             = 0;
 $VERSION    = '0.1-alpha';
 %IRSSI      = (
     authors             => 'kyle isom',
     origauthors         => 'kyle isom',
     contact             => 'coder@kyleisom.net',
     name                => 'geojoin.pl',
-    description         => 'perform geoip city record lookups on joins ' +
+    description         => 'perform geoip city record lookups on joins ' .
                            'in specified channels',
     license             => 'dual-licensed public domain / ISC',
     url                 => 'http://www.brokenlcd.net',
 );
 
 
+if ( $use_city_records && ( ! -s $city_recfile ) ) { 
+    &warn("want to use city records but $city_recfile not found!");
+    return;
+}
+
+else {
+    $enabled = 1;
+}
+
+# set up script variables
+Irssi::settings_add_str('geojoin', 'use_city_records', $use_city_records);
+Irssi::settings_add_str('geojoin', 'city_record_file', $city_recfile);
+my @watchlist   = ( );
+
+
+##### SETTING SUBS #####
+
+# add a channel to the watch list
+sub add_channel {
+    my ($new_channel) = @_;
+    
+    if (grep($new_channel, @watchlist)) {
+        &info("$new_channel already being monitored!");
+        return;
+    } 
+
+    else {
+        push(@watchlist, $new_channel);
+        &info("$new_channel added to watchlist");
+        return; 
+    }
+}
+
+
+##### UTILITY SUBS #####
+sub info {
+    my ($message) = @_;
+    Irssi::print("[+] geojoin: $message");
+}
+
+sub warn {
+    my ($message) = @_;
+    Irssi::print("[!] geojoin: warning - $message");
+}
 
