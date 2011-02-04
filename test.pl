484c4e6b28a675ea6e6c4a586b061f6958e1af3c
Kyle Isom <sadm@kyleisom.net>
2011-02-04 13:20:50 -0700

    finished test script
    test script just dumps a text string to the command window. nothing fancy, kids.

--- test.pl
+++ test.pl
@@ -4,3 +4,19 @@ use strict;
 use warnings;
 
 use Irssi;
+use vars qw($VERSION %IRSSI);
+
+$VERSION='0.0.1';
+%IRSSI = (
+    authors     => 'kyle isom',
+    origauthors => 'kyle isom',
+    contact     => 'coder@kyleisom.net',
+    name        => 'test.pl',
+    description => 'test script to verify Perl::Irssi is loaded',
+    license     => 'dual licensed public domain / ISC',
+    url         => 'NULL',
+);
+
+Irssi::print('[+] test.pl: Irssi module loaded successfully!');
+
+
